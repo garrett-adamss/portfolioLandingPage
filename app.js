e239ac987ae84b1d4d8054fc7cd9604c52ba3fee
Garrett Adams <92003987+garrett-adamss@users.noreply.github.com>
2022-09-15 09:10:44 -0600

    mobile start

--- app.js
+++ app.js
@@ -93,11 +93,8 @@ let aboutTitle = document.getElementById('about-title')
 let aboutText = document.getElementById('about-text')
 
 // Project Text 
-let projectOneTitle = document.getElementById('project-one-title')
-let projectOneType = document.getElementById('project-one-type')
-let projectOneSource = document.getElementById('project-one-source')
-let projectOneVisit = document.getElementById('project-one-visit')
-let projectOneDescription = document.getElementById('project-one-description')
+let projectOneText = document.getElementById('project-one-text')
+
 //Project Photos
 let projectOneImgOne = document.getElementById('project-one-img-one')
 let projectOneImgTwo = document.getElementById('project-one-img-two')
@@ -105,11 +102,8 @@ let projectOneImgThree = document.getElementById('project-one-img-three')
 let projectOneImgFour = document.getElementById('project-one-img-four')
 
 // Project Text 
-let projectTwoTitle = document.getElementById('project-two-title')
-let projectTwoType = document.getElementById('project-two-type')
-let projectTwoSource = document.getElementById('project-two-source')
-let projectTwoVisit = document.getElementById('project-two-visit')
-let projectTwoDescription = document.getElementById('project-two-description')
+let projectTwoText = document.getElementById('project-two-text')
+
 //Project Photos
 let projectTwoImgOne = document.getElementById('project-two-img-two')
 let projectTwoImgTwo = document.getElementById('project-two-img-two')
@@ -131,16 +125,8 @@ window.addEventListener('scroll', function () {
   //PROJECT ONE
   if (viewValue >= 205 && viewValue < 340) {
     { // NOTE Text
-      // @ts-ignore Title
-      projectOneTitle.style.top = ((viewValue - 205) + 2.3) + 'vh';
-      // @ts-ignore Desc
-      projectOneDescription.style.top = ((viewValue - 205) + 10) + 'vh';
-      // @ts-ignore Type
-      projectOneType.style.top = ((viewValue - 205) + 60) + 'vh';
-      // @ts-ignore Source
-      projectOneSource.style.top = ((viewValue - 205) + 56) + 'vh';
-      // @ts-ignore Visit
-      projectOneVisit.style.top = ((viewValue - 205) + 52) + 'vh';
+      // @ts-ignore
+      projectOneText.style.top = ((viewValue - 205)) + 'vh'
     }
     {
       // @ts-ignore Title //56
@@ -158,16 +144,8 @@ window.addEventListener('scroll', function () {
   if (viewValue >= 427 && viewValue < 562) {
     console.log("RUNNING PROJECT TWO");
     { // NOTE Text
-      // @ts-ignore Title
-      projectTwoTitle.style.top = ((viewValue - 427) + 5.3) + 'vh';
-      // @ts-ignore Desc
-      projectTwoDescription.style.top = ((viewValue - 427) + 13) + 'vh';
-      // @ts-ignore Type
-      projectTwoType.style.top = ((viewValue - 427) + 60) + 'vh';
-      // @ts-ignore Source
-      projectTwoSource.style.top = ((viewValue - 427) + 56) + 'vh';
-      // @ts-ignore Visit
-      projectTwoVisit.style.top = ((viewValue - 427) + 52) + 'vh';
+      // @ts-ignore
+      projectTwoText.style.top = ((viewValue - 427)) + 'vh'
     }
     {
       // @ts-ignore Title //56
